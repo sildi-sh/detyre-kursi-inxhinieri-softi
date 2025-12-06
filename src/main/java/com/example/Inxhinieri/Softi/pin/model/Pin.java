fda90750f2079889dbe0e7747cd5ad83e37f92be
sildi-sh <sherollisildi@gmail.com>
2025-12-06 19:34:15 +0100

    create PinTypeEnum and Pin

---
+++ src/main/java/com/example/Inxhinieri/Softi/pin/model/Pin.java
@@ -0,0 +1,39 @@
+package com.example.Inxhinieri.Softi.pin.model;
+
+import com.example.Inxhinieri.Softi.pin.enums.PinType;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "track")
+@Data
+@NoArgsConstructor
+public class Pin {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "CHAR(36)")
+    private String id;
+
+    @Column(name = "track_id", nullable = false, columnDefinition = "CHAR(36)")
+    private String trackId;
+
+    @Column(name="latitude", nullable = false)
+    private double latitude;
+
+    @Column(name="longitude", nullable = false)
+    private double longitude;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="type")
+    private PinType type;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+}
